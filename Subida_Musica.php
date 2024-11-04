5bbcd49e057a6eb63adbf767a58276a8ddd227d0
Cacu <162662695+cacu1212@users.noreply.github.com>
2024-11-04 18:26:19 -0300

    copia lo mismo
    Musica de subida bien subida

--- Subida_Musica.php
+++ Subida_Musica.php
@@ -49,8 +49,8 @@ $limite_canciones = ($categoria === 'EP') ? 6 : (($categoria === 'Sencillo') ? 1
                         <div class="grid-item">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="Generos[]" value="<?php echo htmlspecialchars($genero); ?>" id="genero-<?php echo htmlspecialchars($genero); ?>">
-                                <label class="form-check-label" for="genero-<?php echo htmlspecialchars($genero); ?>">
-                                    <?php echo htmlspecialchars($genero); ?>
+                                <label class="form-check-label form-check-label-subida" for="genero-<?php echo htmlspecialchars($genero); ?>">
+                                <p class="genero-pref"><?php echo htmlspecialchars($genero); ?> </p>
                                 </label>
                             </div>
                         </div>
